1cfc2ab0bfaa4a916d4e62762e5279c9ad27abf7
LamWaiKin <kenlam@smartretail.co>
2023-03-19 10:46:48 +0800

    extract the search screen to another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'apiCaller/bus_routes.dart';
+import 'screens/search.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,111 +16,16 @@ void main() {
   ));
 }
 
-class SearchScreen extends StatefulWidget {
-  const SearchScreen({super.key});
-
-  @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  List<BusRoute> allBusRoutes = [];
-  List<BusCard> displayBusRoutes = [];
-
-  @override
-  void initState() {
-    getKMBBusRoutesList().then((apiData) {
-      setState(() {
-        allBusRoutes = apiData;
-        List<BusCard> fitRoutes = [];
-        for (var busRoute in allBusRoutes) {
-          fitRoutes.add(BusCard(busRote: busRoute));
-        }
-        displayBusRoutes = fitRoutes;
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bus App"),
-        backgroundColor: Colors.black,
-        centerTitle: false,
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextField(
-              onChanged: (inputText) {
-                setState(() {
-                  if (allBusRoutes.isNotEmpty) {
-                    List<BusCard> fitRoutes = [];
-                    for (var busRoute in allBusRoutes) {
-                      if (busRoute.route.contains(inputText)) {
-                        fitRoutes.add(BusCard(busRote: busRoute));
-                      }
-                    }
-                    displayBusRoutes = fitRoutes;
-                  }
-                });
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter a bus number for searching',
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView(
-                padding: const EdgeInsets.all(8), children: displayBusRoutes),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BusCard extends StatelessWidget {
-  final BusRoute busRote;
-
-  const BusCard({super.key, required this.busRote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: const Icon(Icons.directions_bus_sharp),
-            title: Text(busRote.route),
-            subtitle: Text('${busRote.origTc} -> ${busRote.destTc}'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              TextButton(
-                child: const Text('Check Time'),
-                onPressed: () {},
-              ),
-              const SizedBox(width: 8),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class BusScreen extends StatelessWidget {
   const BusScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bus Page"),
+        backgroundColor: Colors.black,
+      ),
+    );
   }
 }
---
+++ lib/screens/search.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../apiCaller/bus_routes.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  List<BusRoute> allBusRoutes = [];
+  List<BusCard> displayBusRoutes = [];
+
+  @override
+  void initState() {
+    getKMBBusRoutesList().then((apiData) {
+      setState(() {
+        allBusRoutes = apiData;
+        List<BusCard> fitRoutes = [];
+        for (var busRoute in allBusRoutes) {
+          fitRoutes.add(BusCard(busRote: busRoute));
+        }
+        displayBusRoutes = fitRoutes;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bus App"),
+        backgroundColor: Colors.black,
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              onChanged: (inputText) {
+                setState(() {
+                  if (allBusRoutes.isNotEmpty) {
+                    List<BusCard> fitRoutes = [];
+                    for (var busRoute in allBusRoutes) {
+                      if (busRoute.route.contains(inputText)) {
+                        fitRoutes.add(BusCard(busRote: busRoute));
+                      }
+                    }
+                    displayBusRoutes = fitRoutes;
+                  }
+                });
+              },
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a bus number for searching',
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+                padding: const EdgeInsets.all(8), children: displayBusRoutes),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BusCard extends StatelessWidget {
+  final BusRoute busRote;
+
+  const BusCard({super.key, required this.busRote});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.directions_bus_sharp),
+            title: Text(busRote.route),
+            subtitle: Text('${busRote.origTc} -> ${busRote.destTc}'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('Check Time'),
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/BusScreen',
+                    arguments: {'targetBus': busRote},
+                  );
+                },
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
